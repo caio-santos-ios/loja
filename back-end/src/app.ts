97418bdc0854c30b1e08fe10b0975dccd1f5fa82
caio-santos-ios <caiosantos.dev@outlook.com>
2024-04-03 13:28:02 -0300

    feat-b: new router order

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -4,7 +4,6 @@ import cors from "cors";
 import { productRouter } from "./routers/product.router";
 import { orderRouter } from "./routers/order.router";
 import middlewares from "./middlewares";
-import { routerTeste } from "./routers/teste";
 
 export const app = express()
 
@@ -12,5 +11,4 @@ app.use(json())
 app.use(cors())
 app.use("/api/products", productRouter)
 app.use("/api/orders", orderRouter)
-app.use("/api/teste", routerTeste)
 app.use(middlewares.handlerErro)
